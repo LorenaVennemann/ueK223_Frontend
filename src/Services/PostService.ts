7f8fd2c60badf14c7ee66af7a5454cbf1800b34c
Yan-P <trash.pishchan@gmail.com>
2024-02-22 16:18:01 +0100

    Create services for the post class

---
+++ src/Services/PostService.ts
@@ -0,0 +1,29 @@
+import api from '../config/Api';
+import { Post } from '../types/models/Post.model';
+
+const PostService = {
+  getPost: async (postID: string): Promise<Post> => {
+    const { data } = await api.get<Post>(`/post/${postID}`);
+    return data;
+  },
+
+  updatePost: (post: Post) => {
+    return api.put(`/post/${post.id}`, post);
+  },
+
+  addPost: (post: Post) => {
+    return api.post('/post/addPost', post).then((res) => {
+      return res.data;
+    });
+  },
+
+  getAllPosts: () => {
+    return api.get(`/post`);
+  },
+
+  deletePost: (id: string) => {
+    return api.delete(`/post/${id}`);
+  },
+};
+
+export default PostService;
---
+++ src/types/models/Post.model.ts
@@ -0,0 +1,8 @@
+export type Post = {
+  id: number;
+  image: any;
+  author: string;
+  author_id: number;
+  likes_count: number;
+  descriptoin: string;
+};
