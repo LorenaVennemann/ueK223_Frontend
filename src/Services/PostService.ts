5ab095455759e9fb77b80a9e969616726bb29566
LorenaVennemann <113357105+LorenaVennemann@users.noreply.github.com>
2024-02-23 15:21:44 +0100

    .

--- src/Services/PostService.ts
+++ src/Services/PostService.ts
@@ -1,20 +1,23 @@
+import { red } from '@mui/material/colors';
 import api from '../config/Api';
 import { Post } from '../types/models/Post.model';
 
 const PostService = {
-  getPost: async (postID: string): Promise<Post> => {
-    const { data } = await api.get<Post>(`/post/${postID}`);
-    return data;
-  },
+
 
   updatePost: (post: Post) => {
     return api.put(`/post/${post.id}`, post);
   },
 
-  addPost: (post: Post) => {
-    return api.post('/post/add', post).then((res) => {
-      return res.data;
-    });
+  addPost: (post : Post) => {
+    return api.post('/post/add', post)
+    .then((response) => {
+      return response.data
+    })
+    .catch((error) => {
+      console.error('Fehler beim Hinzufügen des Posts:', error);
+      throw error;
+      });
   },
 
   getAllPosts: () => {
